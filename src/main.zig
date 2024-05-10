5abbb1686715de5a2cfcb860c5c323cb03294f84
domi <>
2024-05-10 16:47:45 -0400

    added ansi colors and also strings and orginization

--- src/main.zig
+++ src/main.zig
@@ -15,11 +15,11 @@ pub fn main() !void {
     //remove first which is invoked program
     _ = args.next();
     while (args.next()) |arg| {
-        try hexify(arg, 40);
+        try hexify(arg);
     }
 }
 
-fn hexify(filename: []const u8, col: u32) !void {
+fn hexify(filename: []const u8) !void {
 
     //setup buffered writer
     const stdout_file = std.io.getStdOut().writer();
@@ -27,6 +27,7 @@ fn hexify(filename: []const u8, col: u32) !void {
     const stdout = bw.writer();
     errdefer std.debug.print("\nError: parsing file.\n", .{});
 
+    var string_representation: [16]u8 = undefined;
     try stdout.print("File: {s}\n", .{filename});
 
     var file = try std.fs.cwd().openFile(filename, .{});
@@ -36,7 +37,12 @@ fn hexify(filename: []const u8, col: u32) !void {
     defer allocator.free(file_buffer);
     _ = try file.readAll(file_buffer);
     for (file_buffer, 0..) |a, index| {
-        if (index % col == 0) try stdout.print("\n", .{});
+        if (index % 16 == 0) {
+            if (index != 0) try stdout.print("\x1b[90m{s}\x1b[0m", .{string_representation});
+            try stdout.print("\n\x1b[33;2m{x:0>8}\x1b[0m ", .{index});
+            // try stdout.print("\n{d}", .{index});
+        }
+        string_representation[(index % 16)] = if (a > 126 or a < 32) '.' else a;
         try stdout.print("{x:0>2} ", .{a});
     }
     try stdout.print("\n", .{});
